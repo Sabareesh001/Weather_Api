64f90f9a46ed01a6c6b7c0e72a1a6546f9c7e9e2
Hatrick <hatrickda@gmail.com>
2023-09-10 10:38:50 +0530

    Added loop

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
 api_key = '693e0077f4d41e43381f2bc9f0a2eaba'
-city= input("Enter the city : ")
-url = "https://api.openweathermap.org/data/2.5/weather?appid="+api_key+"&q="+city
-Response = requests.get(url).json()
-print(Response)
+while(loop:=True):
+    try:
+        
+            
+        city= input("Enter the city : ")
+        url = "https://api.openweathermap.org/data/2.5/weather?appid="+api_key+"&q="+city
+        Response = requests.get(url).json()
+        print(Response)
 
-Temp =  Response['main']['temp']
-Climate = Response['weather'][0]['main']
-print("/////////////////////////////////////////////////////////////////////////")
-print("Temperature: ",Temp)
-weather_icons = {'01d':'☀️','02d':'⛅','03d':'☁️','04d':'☁️','09d':'🌧️','10d':'🌦️','11d':'🌩️','13d':'❄️','50d':'🌫️',
-                 '01n':'☀️','02n':'⛅','03n':'☁️','04n':'☁️','09n':'🌧️','10n':'🌦️','11n':'🌩️','13n':'❄️','50n':'🌫️'}
-print("Climate: ",Climate, weather_icons.get(Response['weather'][0]['icon']))
+        Temp =  Response['main']['temp']
+        Climate = Response['weather'][0]['main']
+        print("/////////////////////////////////////////////////////////////////////////")
+        print("Temperature: ",Temp)
+        weather_icons = {'01d':'☀️','02d':'⛅','03d':'☁️','04d':'☁️','09d':'🌧️','10d':'🌦️','11d':'🌩️','13d':'❄️','50d':'🌫️',
+                                                 '01n':'☀️','02n':'⛅','03n':'☁️','04n':'☁️','09n':'🌧️','10n':'🌦️','11n':'🌩️','13n':'❄️','50n':'🌫️'}
+        print("Climate: ",Climate, weather_icons.get(Response['weather'][0]['icon']))
 
-print(list(weather_icons))
+        print(list(weather_icons))
+
+        if end:=input(" Ask another (y/n) : ")=="n":
+                  break
+    except:
+        print("Please enter a valid city")
+        
+    
